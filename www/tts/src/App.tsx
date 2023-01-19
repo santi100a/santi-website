ba5cf334c648946519ed6be90939433e63ba0a02
santi100a <santyrojasprieto9@gmail.com>
2023-01-19 16:29:07 -0500

    Aumentada compatibilidad.

--- www/tts/src/App.tsx
+++ www/tts/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
                 id="speed" /><br />
                 <label htmlFor="pitch">Tono: </label>
                 <select defaultValue={1} ref={pitchRef} id="pitch">
-                    <option value="-440">Grave</option>
+                    <option value="0">Grave</option>
                     <option value="1">Medio</option>
                     <option value="2">Agudo</option>
                 </select><br />
